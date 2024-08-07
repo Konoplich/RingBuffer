47f91a9e4bf92106d7bd014ccfbfd700a3b7bcad
konoplich <skonoplich81@gmail.com>
2024-08-07 11:30:56 +0900

    форматирование

--- ringbuffer.h
+++ ringbuffer.h
@@ -4,15 +4,16 @@
 #define RINGBUFFER_ERR_EMPTY (-2)
 #define RINGBUFFER_ERR_FULL (-3)
 
-typedef struct {
-	void* start;
-	void* end;
-	volatile void* readptr; //энергозависимый тип
-	volatile void* writeptr;
+typedef struct
+{
+	void *start;
+	void *end;
+	volatile void *readptr; //энергозависимый тип
+	volatile void *writeptr;
 	volatile int full;
 } ringbuffer;
 
-ringbuffer* ringbuffer_new(unsigned long int capacity, size_t size);
-void ringbuffer_delete(ringbuffer* rb);;
-int ringbuffer_try_write(ringbuffer* rb, void* src, size_t size);
-int ringbuffer_try_read(ringbuffer* rb, void* dst, size_t size);
+ringbuffer *ringbuffer_new (unsigned long int capacity, size_t size);
+void ringbuffer_delete (ringbuffer *rb);;
+int ringbuffer_try_write (ringbuffer *rb, void *src, size_t size);
+int ringbuffer_try_read (ringbuffer *rb, void *dst, size_t size);
